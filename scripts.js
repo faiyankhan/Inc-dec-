07822ce7a3b16edd4fa2e494d81d8e8a0102bd66
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-11-14 15:04:21 +0000

    JS

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const quantElement = document.getElementById('quantity');
+
+function inc() {
+    let quantity = parseInt(quantElement.innerText);
+    quantElement.innerText = ++quantity;
+}
+
+function dec() {
+    let quantity = parseInt(quantElement.innerText);
+    if (quantity>1){
+    quantElement.innerText = --quantity;
+    }
+}
